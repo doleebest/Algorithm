a06fc854a28296de8d272103c6b7460bf4d798d3
Sojeong Lee <90204371+doleebest@users.noreply.github.com>
2024-05-29 14:20:39 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 328 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,26 @@
+while True :
+    sentence = input()
+    stack = []
+    cnt = 0
+    
+    if sentence == '.' : break
+    
+    for i in sentence : 
+        if i == '(' or i == '[':
+            stack.append(i)
+        elif i == ")" :
+            if len(stack) != 0 and stack[-1] == '(' :
+                stack.pop()
+            else :
+                stack.append(')')
+                break
+        elif i == ']' :
+            if len(stack) !=0 and stack[-1] == '[':
+                stack.pop()
+            else :
+                stack.append(']')
+                break
+    if len(stack) == 0 :
+        print('yes')
+    else : 
+        print('no')

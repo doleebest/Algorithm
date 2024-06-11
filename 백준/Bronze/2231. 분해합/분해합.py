394532121e9561da678fbc1b7f5a81d6cb47fef2
Sojeong Lee <90204371+doleebest@users.noreply.github.com>
2024-06-11 23:34:38 +0900

    [Bronze II] Title: 분해합, Time: 1324 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -0,0 +1,8 @@
+import sys
+n = int(sys.stdin.readline())
+result = 0
+for i in range(1,n+1):
+    nums = list(map(int,str(i)))
+    result = sum(nums)+i
+    if result == n : print(i); break
+    if i == n : print(0)

ab0a24d747dfa3b59cefcddb5a6a00f129cc4762
Sojeong Lee <90204371+doleebest@users.noreply.github.com>
2025-02-02 03:05:18 +0900

    [Silver III] Title: 이전 순열, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.py
@@ -0,0 +1,12 @@
+n = int(input())
+a = list(map(int,input().split()))
+for i in range(n-1,0,-1) :
+    if a[i-1] >a[i] :
+        for j in range(n-1,0,-1):
+            if a[i-1]>a[j] :
+                a[i-1], a[j] = a[j], a[i-1]
+                b= sorted(a[i:], reverse=1)
+                a = a[:i] + b
+                print(*a)
+                exit()
+print(-1)

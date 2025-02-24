aea984c2f7cc5189afc219b7a8213ffd5f047527
Sojeong Lee <90204371+doleebest@users.noreply.github.com>
2025-02-24 13:10:15 +0900

    [Gold IV] Title: N-Queen, Time: 29824 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,22 +1,19 @@
-n = int(input())
+def dfs(n):
+    global ans
+    if n==N:    # N행까지 진행한 경우 경우의수 가능: 성공
+        ans+=1
+        return
 
-def attack(x) :
-    for i in range(x) :
-        if row[x]==row[i] or abs(row[x]-row[i]) == abs(x-i):
-            return True
-    return False
+    for j in range(N):
+        if v1[j]==v2[n+j]==v3[n-j]==0:  # 열/대각선 모두 Q 없음
+            v1[j] = v2[n + j] = v3[n - j] = 1
+            dfs(n+1)
+            v1[j] = v2[n + j] = v3[n - j] = 0
 
-def queen(x) :
-    global cnt
-    if x == n :
-        cnt +=1 
-    else :
-        for i in range(n) :
-            row[x] = i
-            if not attack(x):
-                queen(x+1)
-
-row= [0]*n
-cnt = 0
-queen(0)
-print(cnt)
+N = int(input())
+ans = 0
+v1 = [0]*N
+v2 = [0]*(2*N-1)
+v3 = [0]*(2*N-1)
+dfs(0)
+print(ans)

9929a288d6dc5544f533ff7af80fede7e68e7788
Sojeong Lee <90204371+doleebest@users.noreply.github.com>
2025-02-26 13:30:46 +0900

    [Silver III] Title: N과 M (2), Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,14 +1,14 @@
-def dfs(n, lst) :
-  if n>N :             # 종료 조건
-    if len(lst) == M : # M개짜리 수열 => 정답처리
-      ans.append(lst)
+def dfs(n,s,lst) :
+  if n==M:      # M개 선택 완료
+    ans.append(lst)
     return
-
-  dfs(n+1, lst+[n])  # 선택하는 경우
-  dfs(n+1, lst)     # 선택하지 않는 경우
+  
+  for j in range(s, N+1) :
+    dfs(n+1, j+1, lst+[j])
 
 N,M = map(int, input().split())
 ans = []
-dfs(1,[])
-for lst in ans:
-  print(*lst)
+# dfs(1,[])     # 이진트리
+dfs(0,1,[])   # 멀티트리
+for lst in ans :
+  print(*lst) 

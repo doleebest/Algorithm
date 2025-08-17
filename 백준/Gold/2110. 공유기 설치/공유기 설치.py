238bcc298eba571df73cc4df3876408e5c273231
Sojeong Lee <90204371+doleebest@users.noreply.github.com>
2025-08-17 13:21:46 +0900

    [Gold IV] Title: 공유기 설치, Time: 5120 ms, Memory: 40140 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,33 +1,30 @@
-n, c = map(int, input().split())
+n,c = map(int,input().split())
+home = []
+for i in range(n) :
+    home.append(int(input()))
+    
+home.sort()
 
-array = []
-for i in range(n):
-    array.append(int(input()))
-
-array.sort()
-
-def binary_search(array, start, end):
-    while start <= end:
-        mid = (start + end) // 2 	# 현재 공유기 거리.
-        current = array[0] 		 	# 첫 집에 공유기 설치
+def binary_search(home, start, end) :
+    while start<=end : 
+        mid = (start+end)//2
         count = 1
-
-        for i in range(1, len(array)):
-            if array[i] >= current + mid: # 이전 공유기 위치 + mid 
-                count += 1
-                current = array[i]
-
-        if count >= c:		# 너무 많이 설치됐으면
+        current = home[0]
+        
+        for i in range(1,len(home)) :
+            if home[i] >= current+mid :
+                count+=1
+                current = home[i]
+        
+        if count>=c :
             global answer
-            start = mid + 1 # 공유기 사이 거리 늘림
+            start = mid+1
             answer = mid
-        else:
-            end = mid - 1	# 공유기 설치 수가 목표 보다 작으면 공유기 사이 거리 줄임
+        else :
+            end = mid-1
 
-
-start = 1 # 공유기 거리 최소 
-end = array[-1] - array[0] # 공유기 거리 최대
+start = 1
+end = home[-1]-home[0]
 answer = 0
-
-binary_search(array, start, end)
+binary_search(home,start,end)
 print(answer)
